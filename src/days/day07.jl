6737093c6824a9e31fe3302346872b7ad0d10c8c
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-12-07 10:50:54 +0100

    Small tweak to size calculation

--- src/days/day07.jl
+++ src/days/day07.jl
@@ -36,10 +36,7 @@ get_sizes(d::Directory) = (c = IdDict{Directory, Int}(); _size(d, c); c)
 function _size(d::Directory, cache)::Int
     existing = get(cache, d, nothing)
     existing === nothing || return existing
-    s = d.total_filesize
-    for subdir in d.subdirs
-        s += _size(subdir, cache)
-    end
+    s = d.total_filesize + mapreduce(i -> _size(i,cache), +, d.subdirs; init=d.total_filesize)
     cache[d] = s
     s
 end
